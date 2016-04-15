5e1e2ad70a0274467bb5d842a398863e8fff0363
Godje TV <comepot.4anell@gmail.com>
2016-04-14 21:42:50 -0700

    =

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-var body, html, sliderBody, btnLeft, btnRight, i, parts, vHeight, vWidth;
+var body, html, sliderBody, btnLeft, btnRight, i, parts,
+    vHeight, vWidth, slide, slideBlock;
 //Variable definitions
 var i = 0,
-    parts = 3,
+    parts = 7,
 
     //Main html elements
     body = document.body,
@@ -16,18 +17,35 @@ var i = 0,
     btnRight = _id("btn-right"),
 
     urls = ["http://www.wallpapereast.com/static/images/pier_1080.jpg",
+            "http://www.wallpapereast.com/static/images/abstract-hd-wallpaper-338453.jpg",
             "http://www.wallpapereast.com/static/images/pier_1080.jpg",
-            "http://www.wallpapereast.com/static/images/pier_1080.jpg",
-            "http://www.wallpapereast.com/static/images/pier_1080.jpg"];
+            "http://www.wallpapereast.com/static/images/abstract-hd-wallpaper-338453.jpg"];
 
-    slide = _createEl("div");
-    slide.className += "slide-el";
+window.addEventListener("resize", function(){
+  vHeight = window.innerHeight;
+  vWidth = window.innerWidth;
 
-var slideBlock = _createEl("div");
-    slideBlock.className += "slide-block slide-block-";
+  slideBlockCreate()
+}, false);
 
 window.onload = function(){
+  createElements();
   slideLayout();
+}
+function createElements(){
+  slide = _createEl("div");
+  slide.className += "slide-el";
+
+  slideBlockCreate();
+}
+
+function slideBlockCreate(){
+  console.log(vHeight, vWidth);
+
+  slideBlock = _createEl("div");
+  slideBlock.className += "slide-block slide-block-";
+  slideBlock.style.width = vWidth + "px";
+  slideBlock.style.height = (vHeight / parts ) + "px";
 }
 
 function slideLayout(){
@@ -36,11 +54,33 @@ function slideLayout(){
   };
   var slides = _cl("slide-el");
   for(var i=0; i < slides.length; i++){
-    for(var x=0; x < 3; x++){
+    for(var x=0; x < parts; x++){
       slides[i].appendChild(slideBlock.cloneNode(true))
+      slides[i].className = "slide-el slide-el-"+ i +""
+    }
+  };
+  for(var i = 0; i < slides.length; i++){
+    console.log(urls[i]);
+    for(var x=0;x<slides[i].children.length;x++){
+      slides[i].children[x].style["background-image"] = "url("+urls[i]+")";
     }
   }
 }
+
+//Select each slide
+//Select it's children
+//Each children suppose to have the same bgi that parent has.
+
+
+
+
+
+
+
+
+
+
+
 
 function _id(el){
   return document.getElementById(""+ el +"");
