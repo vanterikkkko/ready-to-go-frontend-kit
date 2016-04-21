97e883f4e5bccab09cceb7cb41e2bad5c5226040
Godje TV <comepot.4anell@gmail.com>
2016-04-20 18:32:04 -0700

    Final commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
-var body, html, sliderBody, btnLeft, btnRight, i, parts,
-    vHeight, vWidth, slide, slideBlock, slides
-//Variable definitions
+var sliderBody, btnLeft, btnRight, i, parts,
+    vHeight, vWidth, slideBlock, parts, slides, slide;
+//Variable definition
 var i = 0,
-    parts = 1,
-
-    //Main html elements
-    body = document.body,
-    html = document.element,
-
+    parts = 3,
 
     sliderBody = _id("slider"),
     btnLeft = _id("btn-left"),
@@ -24,35 +19,30 @@ var i = 0,
             "http://cs627529.vk.me/v627529202/3c7d4/ST7Ia7JC_lo.jpg",
             "http://cs630117.vk.me/v630117202/22b1f/ILT1HdQZoqk.jpg",
             "http://cs629301.vk.me/v629301202/4ac5e/u1Ed3_qEn-g.jpg",
-            ];
+          ];
 
+window.onload = function(){
+  createElements();
+  //autoSlide();
+}
 window.addEventListener("resize", function(){
   vHeight = window.innerHeight;
   vWidth = window.innerWidth;
 
-  slideBlockCreate();
-}, false);
-
-window.onload = function(){
   createElements();
-}
+});
+
 function createElements(){
   slide = _createEl("div");
   slide.className += "slide-el";
 
-  slideBlockCreate();
-}
-
-function slideBlockCreate(){
-  console.log(vHeight, vWidth);
-
   slideBlock = _createEl("div");
   slideBlock.className += "slide-block slide-block-";
   slideBlock.style.width = vWidth + "px";
-  slideBlock.style.height = (vHeight / parts ) + "px";
+  slideBlock.style.height = (vHeight / parts) + "px";
   slideBlock.style["transition"] = "left "+ transitionTime +"s";
 
-  slideLayout();
+  slideLayout()
 }
 
 function slideLayout(){
@@ -63,23 +53,19 @@ function slideLayout(){
   var slides = _cl("slide-el");
   for(var i=0; i < slides.length; i++){
     for(var x=0; x < parts; x++){
-      slides[i].appendChild(slideBlock.cloneNode(true))
-      slides[i].className = "slide-el slide-el-"+ i +""
+      slides[i].appendChild(slideBlock.cloneNode(true));
     }
+    slides[i].className += " slide-el-"+ i +"";
   };
   for(var i = 0; i < slides.length; i++){
-    console.log(slides[i]);
     slides[i].style.zIndex = i;
-    for(var x=0;x<slides[i].children.length;x++){
+    slides[i].style["transition-time"] = transitionTime+"s";
+    for(var x=0; x < slides[i].children.length; x++){
       var offset = (x*(100/parts));
-      var imageOffset = vHeight / offset;
       var transitionDelay = ((transitionTime / parts)/2)*x;
-      console.log(transitionDelay);
       slides[i].children[x].style.top = offset + "%";
-      slides[i].children[x].innerHTML = "<img src="+ urls[i] +" style='top: "+(((vHeight / parts)*-1)*x)+"px'>"
-      // slides[i].children[x].style["background-image"] = "url("+urls[i]+")";
-      // slides[i].children[x].style["background-position"] = "50% "+((((vHeight / parts)*-1)*x))/+"px";
-      slides[i].children[x].style["transition-delay"] = transitionDelay +"s";
+      slides[i].children[x].innerHTML = "<img src="+ urls[i] +" style='top: "+ ((vHeight / parts)*-1)*x+"px'></img>";
+      slides[i].children[x].style["transition-delay"] = transitionDelay + "s";
     }
   }
   addStyle();
@@ -87,51 +73,60 @@ function slideLayout(){
 
 function addStyle(){
   var slides = _cl("slide-el");
-  console.log(slides[i]);
   slides[i].className += " opened";
 }
 
 btnLeft.addEventListener("click", function(){
   goLeft();
-});
+})
+
 function goLeft(){
   var slides = _cl("slide-el");
   if(i > 0){
     i--;
-    removeClass(slides[i+1], "opened")
+    removeClass(slides[i+1], "opened");
   } else {
     i = urls.length - 1;
-    for(x=0;x<urls.length;x++){
-      slides[x].className += " opened"
+    for(x=0; x < urls.length; x++){
+      slides[x].className += " opened";
     }
   }
+  console.log(i);
 }
 btnRight.addEventListener("click", function(){
   goRight();
-});
+})
 
 function goRight(){
-  var slides = _cl("slide-el");
+  var slides= _cl("slide-el");
   if(i < urls.length -1){
     i++;
-    slides[i].className += " opened"
+    slides[i].className += " opened";
   } else {
     i = 0;
-    for(x = urls.length; x > 0 ; x--){
+    for(x = urls.length; x > 0; x--){
       removeClass(slides[x], "opened");
     }
   }
 }
 
-//Helper functions
+function autoSlide(){
+  setInterval(function(){
+    goRight();
+  }, 5000);
+}
+
+
+
+
 function _id(el){
-  return document.getElementById(""+ el +"");
+  return document.getElementById(el);
 }
 function _cl(el){
-  return document.getElementsByClassName(""+ el +"")
+  return document.getElementsByClassName(el)
 }
 function _createEl(el){
-  return document.createElement(""+ el +"");
+  return document.createElement(el);
 }
 function removeClass(el, _class) {
   if (el && el.className && el.className.indexOf(_class) >= 0) {
