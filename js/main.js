a2eeb1eb0aa50d0d75607ac3bf091d9bf1a9c94b
Godje TV <comepot.4anell@gmail.com>
2016-05-18 18:23:05 -0700

    After 2 videos

--- js/main.js
+++ js/main.js
@@ -1,60 +1,29 @@
-window.onload = function(){
-  init()
-}
-
-function init(){
-  var owl = $(".owl-carousel"),
-      nextBtn = $("#right-btn"),
-      rightBtn = $("#left-btn"),
-      backgroundHolder = $("#background-holder");
-  carousel();
-
-  owl.on("changed.owl.carousel", function(e){
-    var page = e.page.index,
-        pageSuspect, suspectURL;
-    setTimeout(function(){
-      pageSuspect = owl.find(".owl-item.active").children()
-      suspectURL = pageSuspect.attr("src");
-      backgroundHolder.css({"background-image": "url('" + suspectURL + "')"})
-      console.log(suspectURL)
-    }, 20)
-  });
-
-
-  nextBtn.on("click", function(){
-    owl.trigger("next.owl.carousel", [400])
-  });
-
-  rightBtn.on("click", function(){
-    owl.trigger("prev.owl.carousel", [400])
-  })
-}
-
-var carousel = function() {
-  var owl = $(".owl-carousel");
-  owl.owlCarousel({
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
     loop: true,
-    center: true,
-    items: 1,
-    mouseDrag: true,
-    touchDrag: true,
-    autoWidth: true,
+    +center: true,
+    margin: 10,
+    stagePadding: 20,
     responsive: {
       0: {
-        stagePadding: 0,
-        margin: 15,
         items: 1,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplaySpeed: 500,
+        smartSpeed: 1000
       },
       640: {
-        stagePadding: 50,
-        margin: 35,
-        items: 1
+        items: 2,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2000
       },
-      1280: {
-        stagePadding: 150,
-        margin: 130,
-        items: 1
+      800: {
+        items: 3,
+        touchDrag: true,
+        loop: false
       }
     }
-  });
-}
+  })
+});
